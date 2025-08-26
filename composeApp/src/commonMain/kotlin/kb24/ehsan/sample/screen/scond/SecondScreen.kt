ada4403c690034b63ed600015e1de273a731f1e3
ehsan zarghalami <e.zarghalami@hamrahcard.ir>
2025-08-26 08:18:43 +0330

    add simple ui for second page

--- composeApp/src/commonMain/kotlin/kb24/ehsan/sample/screen/scond/SecondScreen.kt
+++ composeApp/src/commonMain/kotlin/kb24/ehsan/sample/screen/scond/SecondScreen.kt
@@ -1,7 +1,39 @@
 package kb24.ehsan.sample.screen.scond
 
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 
 @Composable
 fun SecondScreen() {
+    SecondLayout()
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+private fun SecondLayout() {
+    Scaffold(
+        modifier = Modifier.fillMaxSize(),
+        topBar = {
+            CenterAlignedTopAppBar(
+                title = {
+                    Text(text = "second")
+                }
+            )
+        }
+    ) { paddingValues ->
+        Box(modifier = Modifier.fillMaxSize().padding(paddingValues)) {
+            CircularProgressIndicator(
+                modifier = Modifier.align(Alignment.Center)
+            )
+        }
+    }
 }
